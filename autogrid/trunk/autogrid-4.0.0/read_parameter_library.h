a3f6b02bcc561af1b4e1e747d299dde103aa9d2e
eosie <eosie@7164d5a2-a89c-11de-9ed3-3f1bf8abe145>
2008-11-01 18:11:21 +0000

    - directory restructuring #4: autogrid/trunk has been deleted

--- autogrid/trunk/autogrid-4.0.0/read_parameter_library.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <cstdio>
-
-void read_parameter_library(char FN_parameter_library[MAX_CHARS], int outlev, const char *programname, int debug, FILE *logFile, Linear_FE_Model &AD4);
-void setup_parameter_library(int outlev, const char *programname, int debug, FILE *logFile, Linear_FE_Model &AD4);
